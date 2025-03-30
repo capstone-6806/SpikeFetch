1eeea2a705bd3a2e35894da0c954484798d4448f
Kyrie Lin <kyrielin66@gmail.com>
2025-03-30 00:52:19 -0700

    feat: update run_spikefetch_trace

--- run_spikefetch_trace.sh
+++ run_spikefetch_trace.sh
@@ -5,28 +5,17 @@ mkdir -p spikefetch_prefetches_traces
 # script to run spikefetch on cloudsuite and spec
 find "./traces" -type f -name "*.txt.xz" | while read xz; do
   file_variable="$(basename $xz .txt.xz)"
-  file_cc="cc-5"
   file_605mcf="605.mcf-s1"
-  file_bfs10="bfs-10"
   file_450soplex="450.soplex-s0"
   file_623xalan="623.xalancbmk-s1"
   file_471omnetpp="471.omnetpp-s1"
   file_482sphinx3="482.sphinx3-s0"
   file_473astar="473.astar-s1"
 
-
   ./ml_prefetch_sim.py generate "$xz" ./spikefetch_prefetches_traces/prefetch_14th_rerun_"$(basename "$xz" .txt.xz)".txt --model temp
-
-  if [[ "$file_variable" == "$file_cc" ]]; then
-    ./ml_prefetch_sim.py run ./traces/"$file_variable".trace.gz --prefetch ./spikefetch_prefetches_traces/prefetch_14th_rerun_"$file_variable".txt --num-instructions 20700644
-  fi
 
   if [[ "$file_variable" == "$file_605mcf" ]]; then
     ./ml_prefetch_sim.py run ./traces/"$file_variable".trace.xz --prefetch ./spikefetch_prefetches_traces/prefetch_14th_rerun_"$file_variable".txt --num-instructions 38467912
-  fi
-
-  if [[ "$file_variable" == "$file_bfs10" ]]; then
-    ./ml_prefetch_sim.py run ./traces/"$file_variable".trace.gz --prefetch ./spikefetch_prefetches_traces/prefetch_14th_rerun_"$file_variable".txt --num-instructions 61219343
   fi
 
   if [[ "$file_variable" == "$file_450soplex" ]]; then
